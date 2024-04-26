abac0c3f2526cdac52721fd6a53e4441b4ff30c7
NathanChabloz <nathan.chabloz@eduvaud.ch>
2024-04-26 13:52:09 +0200

    Voila mon avancée

--- APP_FILMS_164/genres/gestion_genres_crud.py
+++ APP_FILMS_164/genres/gestion_genres_crud.py
@@ -34,26 +34,26 @@ def genres_afficher(order_by, id_genre_sel):
         try:
             with DBconnection() as mc_afficher:
                 if order_by == "ASC" and id_genre_sel == 0:
-                    strsql_genres_afficher = """SELECT id_genre, intitule_genre, date_ins_genre FROM t_genre ORDER BY id_genre ASC"""
-                    mc_afficher.execute(strsql_genres_afficher)
+                    strsql_categore_afficher = """SELECT * FROM t_catégorie ORDER BY id_categorie ASC"""
+                    mc_afficher.execute(strsql_categore_afficher)
                 elif order_by == "ASC":
                     # C'EST LA QUE VOUS ALLEZ DEVOIR PLACER VOTRE PROPRE LOGIQUE MySql
                     # la commande MySql classique est "SELECT * FROM t_genre"
                     # Pour "lever"(raise) une erreur s'il y a des erreurs sur les noms d'attributs dans la table
                     # donc, je précise les champs à afficher
                     # Constitution d'un dictionnaire pour associer l'id du genre sélectionné avec un nom de variable
-                    valeur_id_genre_selected_dictionnaire = {"value_id_genre_selected": id_genre_sel}
-                    strsql_genres_afficher = """SELECT id_genre, intitule_genre, date_ins_genre  FROM t_genre WHERE id_genre = %(value_id_genre_selected)s"""
+                    valeur_id_categorie_selected_dictionnaire = {"value_id_categorie_selected": id_genre_sel}
+                    strsql_categore_afficher = """SELECT * FROM t_catégorie ORDER BY id_categorie WHERE id_categorie = %(value_id_categorie_selected)s"""
 
-                    mc_afficher.execute(strsql_genres_afficher, valeur_id_genre_selected_dictionnaire)
+                    mc_afficher.execute(strsql_categore_afficher, valeur_id_categorie_selected_dictionnaire)
                 else:
-                    strsql_genres_afficher = """SELECT *  FROM t_genre ORDER BY id_genre DESC"""
+                    strsql_categore_afficher = """SELECT * FROM t_catégorie ORDER BY id_categorie DESC"""
 
-                    mc_afficher.execute(strsql_genres_afficher)
+                    mc_afficher.execute(strsql_categore_afficher)
 
                 data_genres = mc_afficher.fetchall()
 
-                print("data_genres ", data_genres, " Type : ", type(data_genres))
+                print("data_categorie ", data_genres, " Type : ", type(data_genres))
 
                 # Différencier les messages si la table est vide.
                 if not data_genres and id_genre_sel == 0:
